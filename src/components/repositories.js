3ca3e555009d709318cb4b70c4f145d437b604a3
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-03-31 13:27:33 +0400

    search and reverse repository list

--- src/components/repositories.js
+++ src/components/repositories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Repo from './repo';
 import Loading from '../images/repo_loading.gif';
+import _ from 'lodash';
 
 import '../stylesheets/repositories.css';
 
@@ -18,20 +19,38 @@ class Repositories extends React.PureComponent {
     try {
       const res = await fetch(this.props.reposUrl);
       const repos = await res.json();
-      this.setState({repos, loading: false});
+      this.setState({repos, allRepos: [...repos], loading: false});
     } catch (e) {}
+  }
+
+  reverse = () => {
+    const {repos} = this.state;
+    const reversedRepos = [...repos].reverse();
+    this.setState({repos: reversedRepos});
+  };
+
+  search = e => {
+    const text = e.target.value;
+    const filtered = _.filter(this.state.allRepos, (repo) => {
+      return repo.name.toLowerCase().includes(text.toLowerCase());
+    });
+    this.setState({repos: filtered});
   }
 
   render(){
     const {loading, repos} = this.state;
 
     return (
-      <div className="repositories">{
-        loading ? <img src={Loading} alt="" /> : 
-          repos.map(repo =>(
-            <Repo {...{key: repo.name, repo, userName: this.props.userName}} />
-          ))
-      }</div>
+      <div className="repositories">
+        <button onClick={this.reverse}>Reverse</button>
+        <input onChange={this.search} placeholder="Search"/>
+        {
+          loading ? <img src={Loading} alt="" /> : 
+            repos.map(repo =>(
+              <Repo {...{key: repo.name, repo, userName: this.props.userName}} />
+            ))
+        }
+      </div>
     );
 
     
